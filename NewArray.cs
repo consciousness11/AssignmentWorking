9ac6d431ca35039a3f3aff120c3cd441e736eb47
Marut <marut.dhungana@gmail.com>
2021-05-16 22:12:14 +0545

    Add array in NumArray

---
+++ NewArray.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStuffs
+{
+    class NumArray
+    {
+        public void sumArray(int[] array)
+        {
+            int total = array.Sum();
+            Console.WriteLine("Sum of array is  " + total);
+
+        }
+               
+    }
+}
